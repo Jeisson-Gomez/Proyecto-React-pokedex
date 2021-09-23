ca7dae836bd1f65b96220e4adf4d0e5e11f6c245
Jeisson Gomez <71471533+jeypitbull@users.noreply.github.com>
2021-09-22 20:02:19 -0500

    Actualizacion del proyecto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import NavBar from './components/layout/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>jeisson Gomez</h1>
+      <NavBar/>
     </div>
   );
 }
